87901a10d1380403f568c0533d8628e14c26c764
Paweł <pawel.janik.1983@gmail.com>
2024-02-20 16:56:40 +0100

    API fetch and JSON hanling working

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+import fetchWrapper from "./fetch-wrapper.js";
+
+// API URL  	https://api.adviceslip.com/advice
+
+const advice_id = document.querySelector("#advice_Id");
+const advice_text = document.querySelector("#advice_Text");
+const dice = document.querySelector("#roll-the-dice");
+
+const getAdvice = () => {
+    const API = new fetchWrapper("https://api.adviceslip.com");
+
+    API.get("/advice").then((data) => {
+        advice_id.textContent = `Advice #${data.slip.id}`;
+        advice_text.textContent = `"${data.slip.advice}"`;
+        // console.log(data);
+    });
+};
+
+dice.addEventListener("click", () => {
+    getAdvice();
+});
